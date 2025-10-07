f03dbce1fd699b69e53f0059ef3d3a884957792b
ehdnd_mac <taeungjang@naver.com>
2025-10-07 22:50:34 +0900

    6 2 Deps

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -10,8 +10,14 @@ function App() {
   const onClick = () => setCount((cur) => cur + 1);
   const onChange = (event) => setKeyword(event.target.value);
 
-  console.log("run");
-  console.log("search API call:", keyword);
+  console.log("run everythime");
+  useEffect(() => console.log("API call"), []);
+
+  // keyword 가 바뀔 때에만 이 코드를 호출
+  // 즉 counter 변화할 때에는 이 코드 호출 안함
+  useEffect(() => {
+    if (keyword && keyword.length > 5) console.log("search for:", keyword);
+  }, [keyword]);
 
   return (
     <div>
