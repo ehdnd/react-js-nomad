a3bb43078b25df6fc00c558aba387d644f7b6858
ehdnd_mac <taeungjang@naver.com>
2025-10-07 23:23:08 +0900

    6 3 Cleanup

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,31 +1,26 @@
 import { useState, useEffect } from "react";
 
 import Btn from "./Btn";
-import styles from "./App.module.css";
+
+function Greeting() {
+  // Greeting 컴포넌트를 hide == 화면에서 지워버린다
+  useEffect(() => {
+    console.log("created");
+    return () => {
+      console.log("deleted");
+    };
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () => setCount((cur) => cur + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("run everythime");
-  useEffect(() => console.log("API call"), []);
-
-  // keyword 가 바뀔 때에만 이 코드를 호출
-  // 즉 counter 변화할 때에는 이 코드 호출 안함
-  useEffect(() => {
-    if (keyword && keyword.length > 5) console.log("search for:", keyword);
-  }, [keyword]);
+  const [show, setShow] = useState(false);
+  const onClick = () => setShow((cur) => !cur);
 
   return (
     <div>
-      <input type="text" onChange={onChange} />
-      <h1 className={styles.title}>{keyword}</h1>
-      <Btn text={"Search"}></Btn>
-      <h1 className={styles.title}>{count}</h1>
-      <Btn text={"countup"} onClick={onClick}></Btn>
+      {show ? <Greeting> </Greeting> : null}
+      <Btn onClick={onClick} text={show ? "Hide" : "Show"}></Btn>
     </div>
   );
 }
